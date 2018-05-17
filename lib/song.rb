048bbbad752b6d8b02b2f59f2502b5302c742775
Jathmel Gordon <jathmel@gmail.com>
2018-05-17 01:15:59 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song 
+  attr_reader :name, :artist, :genre 
+  @@count = 0
+  @@artist = []
+  def self.count
+    @@count
+  end 
+  def self.artists
+  end
+  def self.genres 
+  end
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist 
+    @genre = genre
+    @@count += 1
+    @@artist << artist
+  end
+  
+    
+end
